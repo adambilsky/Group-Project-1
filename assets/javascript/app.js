1395d0577936167066307072d3a26392f34be0f7
ulas <udegertekin@gmail.com>
2018-07-14 11:43:15 -0500

    index change and js attached to index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,3 +45,23 @@ var CAArray = [
     }
 
 ]
+$.ajax({
+    url: "https://data.cityofchicago.org/resource/6zsd-86xi.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "chp9vzClkoQ3bf0yZLoCpG21u"
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  for (i=0; i<100; i++) {
+    if (data[i].community_area === 7) {
+        var lpdata = data[i].community_area;
+        console.log(lpdata);
+    }
+    // else {
+    //     alert("There are no records matching the criteria you have chosen.")
+    // }
+  console.log(data);
+
+}});    
