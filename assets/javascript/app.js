e945163dd37df63d9e6291f0e75c5166874ead55
Adam Bilsky <adambilsky@gmail.com>
2018-07-20 09:08:08 -0500

    added description row to table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,28 +15,18 @@ var database = firebase.database();
 
 // An array of objects, each containing one of 
 // the 10 neighborhood names and corresponding Community Area #
-var CAArray = [
-    {"Edgewater": 54873
-    },
-    {"Hyde Park": 26893
-    },
-    {"Lakeview": 98212
-    },
-    {"Lincoln Park": 66959
-    },
-    {"Lincoln Square": 40761
-    },
-    {"Logan Square": 73702
-    },
-    {"Pilsen (Lower West Side)": 34410
-    },
-    {"Uptown": 55137
-    },
-    {"Bucktown (West Town)": 86429
-    },
-    {"Wicker Park (West Town)": 86429
+var CAArray = {
+    "Edgewater": 54873,
+    "Hyde Park": 26893,
+    "Lakeview": 98212,
+    "Lincoln Park": 66959,
+    "Lincoln Square": 40761,
+    "Logan Square": 73702,
+    "Pilsen (Lower West Side)": 34410,
+    "Uptown": 55137,
+    "Bucktown (West Town)": 86429,
+    "Wicker Park (West Town)": 86429
     }
-]
 
 // An event listener on the click/drop-down menu 
 // to pass the Community Area value through the API 
@@ -51,7 +41,6 @@ $(".neighborhood").on("click", function () {
     // in each reported neighborhood AND the number of each type committed in 2018.
 
     var crimeTypes = {};
-    var crimeNames = [];
     var CA = $(this).attr("data-value");
     var neighName = $(this).attr("data-name");
     $.ajax({
@@ -85,9 +74,8 @@ $(".neighborhood").on("click", function () {
         console.log(crimeTypes);
         
         // List the types of crime
-        console.log("Folliwing are all crime types reported out of " + data.length + " records retrieved from " + neighName + ": ");
+        console.log("Following are all crime types reported out of " + data.length + " records retrieved from " + neighName + ": ");
         console.log(Object.keys(crimeTypes));
-        console.log(Object.getOwnPropertyNames(crimeTypes));
 
         //Grab one particular crime type and name it by index position 
         console.log("Following is the first crime type recorded in the list of records returned from " + neighName +": ");
@@ -96,33 +84,38 @@ $(".neighborhood").on("click", function () {
         //Report one particular type of crime from the neighborhood by name and list the number
         console.log("Following is the number of Assaults reported in " + neighName + ": " + crimeTypes.ASSAULT);
         console.log("Following is the number of Batteries reported in " + neighName + ": " + crimeTypes.BATTERY);
-        
         console.log("Following is the number of Motor Vehicle Thefts reported in " + neighName + ": " + crimeTypes["MOTOR VEHICLE THEFT"]);
        
-        // console.log(Object.values(crimeTypes));
         console.log(Object.entries(crimeTypes));
-        
-        // The following determines the most frequent crime type in the report, 
-        // and returns its key.
+        var population = parseInt(CAArray[neighName]);
 
-        var max = 0;
-        var maxKey;
-        var crimeArr = [];
+        var maxCrime = function () {
+            // The following determines the most frequent crime type in the report, 
+            // and returns its key.
 
-        for(var key in crimeTypes) {
-            if(crimeTypes[key] > max){
-                max = crimeTypes[key];
-                maxKey = key;
-            }    
+            var max = 0;
+            var maxKey;
+            var crimeArr = [];
+
+            for(var key in crimeTypes) {
+                if(crimeTypes[key] > max){
+                    max = crimeTypes[key];
+                    maxKey = key;
+                }    
+            }
+            console.log(maxKey + ": " + max + "/" + data.length);
+            var crimeRate = Math.round((max/data.length)*100).toFixed(0);
+            var crimePerCapita = ((max/population)*100).toFixed(2);
+            console.log("Homicides in " + neighName + ": " + crimeTypes["HOMICIDE"]);
+            console.log;
+            $("#crime-display-table > tbody").append("<tr><td>" + neighName + "</td><td>" + population + "</td><td>" + maxKey + "</td><td>" + crimeRate + "</td><td style = 'color: red'>" + crimeTypes["HOMICIDE"] + "</td></tr>" + "<tr><td>" + "The most frequent crime in " + neighName + " is " + maxKey + ", which occurs at a rate of " + crimePerCapita + "% per capita, and which comprised " + crimeRate + "% (" + max + "/" + data.length + ") of all crimes in 2018." + "</td></tr>");
         }
-
+        maxCrime();
+        
+        // Goal: to compare the neighborhood crime data to population size
+        // in order to create a more user-oriented neighborhood "snapshot"
         console.log(CAArray)
-
-        console.log(maxKey + ": " + max + "/" + data.length);
-        var crimeRate = Math.round((max/data.length)*100).toFixed(0);
-        console.log("Homicides in " + neighName + ": " + crimeTypes["HOMICIDE"]);
-        console.log("The most frequent crime in " + neighName + " is " + maxKey + ", which comprised " + crimeRate + "% (" + max + "/" + data.length + ") of all crimes in 2018.");
-        $("#crime-display-table > tbody").append("<tr><td>" + neighName + "</td><td>" +maxKey + "</td><td>" + crimeRate + "</td><td style = 'color: red'>" + crimeTypes["HOMICIDE"] + "</td></tr>");
+        console.log("Population of " + neighName + ": " + CAArray[neighName]);
 
         // database.ref().push({
         //     neighName,
@@ -134,5 +127,3 @@ $(".neighborhood").on("click", function () {
 // database.ref().on("child_added", function (childSnapshot) {
 //     console.log(childSnapshot.val());
 // });
-// Upcoming (2): A dynamic table creation function 
-// to push the results of a search into a readable format.
